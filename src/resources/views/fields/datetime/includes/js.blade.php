e0466c060d293ffc57063a79971d52b56e203e51
Aden Fraser <aden.fraser@bc-legal.co.uk>
2019-02-06 21:29:11 +0000

    Additional options for Datetime picker

--- src/resources/views/fields/datetime/includes/js.blade.php
+++ src/resources/views/fields/datetime/includes/js.blade.php
@@ -6,6 +6,7 @@
                 timePickerIncrement: {{ isset($options['increment']) ? $options['increment'] : '30' }},
                 singleDatePicker: {{ isset($options['range']) && $options['range'] == true ? 'false' : 'true' }},
                 format: {!! isset($options['format']) ? $options['format'] : "'YYYY/MM/DD HH:mm:ss'" !!},
+                timePicker12Hour: {{ isset($options['timePicker24Hour']) ? 'false' : 'true' }},
             }
         );
     </script>
