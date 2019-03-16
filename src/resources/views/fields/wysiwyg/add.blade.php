5a4f9cbfdcdf56ffe4f4130c833f34bab88ae8c4
Aden Fraser <aden.fraser@bc-legal.co.uk>
2019-03-16 16:25:59 +0000

    Far better WYSIWYG

--- src/resources/views/fields/wysiwyg/add.blade.php
+++ src/resources/views/fields/wysiwyg/add.blade.php
@@ -32,9 +32,20 @@
 </div>
 
 @section('enqueued-js')
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.css" rel="stylesheet">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
+
+    <style>
+        .note-group-select-from-files {
+            display: none;
+        }
+    </style>
+
     <script>
         $(function () {
-            $("#{{ $attribute }}").wysihtml5();
+            $("#{{ $attribute }}").summernote({
+                minHeight: 200,
+            });
         });
     </script>
 @append 
